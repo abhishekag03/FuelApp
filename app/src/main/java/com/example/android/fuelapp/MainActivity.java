51ea57429cca9a683c55613d838a68efcfecd280
Vishaal <vishaal16119@iiitd.ac.in>
2017-06-28 14:03:10 +0530

    Added Location Functionality.

--- app/src/main/java/com/example/android/fuelapp/MainActivity.java
+++ app/src/main/java/com/example/android/fuelapp/MainActivity.java
@@ -2,47 +2,82 @@ package com.example.android.fuelapp;
 
 import android.*;
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-{
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-    private static final int REQUEST_LOCATION_PERMISSION=122;
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements com.google.android.gms.location.LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int REQUEST_LOCATION_PERMISSION = 122;
+
+    public String TAG = "database";
+
+    private static final long INTERVAL = 10000;
+    private static final long FASTEST_INTERVAL = 5000;
+
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
 
 
-    private void askForPermission(final String permission, final Integer requestCode)
-    {
-        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) != PackageManager.PERMISSION_GRANTED)
-        {
+    private TextView currentFuelTextview;
+    private TextView favouriteFuelTextView;
+    private TextView mostTimesFuelTextView;
+    private TextView previousFuelTextView;
+
+    private Button fillButton;
+
+    private void askForPermission(final String permission, final Integer requestCode) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permission)) {
 
                 //This is called if user has denied the permission before
                 //In this case I am just asking the permission again
 
-                    AlertDialog.Builder alertDialog=new AlertDialog.Builder(this);
-                    alertDialog.setMessage("Location Permission is required for this app to work.");
-                    alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
-                        }
-                    });
-                    alertDialog.setNegativeButton("Cancel", null);
-                    AlertDialog a=alertDialog.create();
-                    a.show();
-                    Window window= a.getWindow();
-                    window.setLayout(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+                alertDialog.setMessage("Location Permission is required for this app to work.");
+                alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+                    }
+                });
+                alertDialog.setNegativeButton("Cancel", null);
+                AlertDialog a = alertDialog.create();
+                a.show();
+                Window window = a.getWindow();
+                window.setLayout(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
 
 
             } else {
@@ -55,29 +90,158 @@ public class MainActivity extends AppCompatActivity
     }
 
 
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         askForPermission(Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_LOCATION_PERMISSION);
+
+
+        currentFuelTextview = (TextView) findViewById(R.id.current_fuel_cost);
+        favouriteFuelTextView = (TextView) findViewById(R.id.favourite_fuel_cost);
+        mostTimesFuelTextView = (TextView) findViewById(R.id.most_used_fuel_cost);
+        previousFuelTextView = (TextView) findViewById(R.id.last_fuel_cost);
+
+        fillButton=(Button) findViewById(R.id.fill_fuel_button);
+
+        if (!GooglePlayServicesAvailable()) {
+            finish();
+        }
+
+        createLocationRequest();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+
+        fillButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUI();
+            }
+        });
+
+
     }
+
+    private void updateUI()
+    {
+        if(mCurrentLocation!=null){
+            String lat= String.valueOf(mCurrentLocation.getLatitude());
+            String lng= String.valueOf(mCurrentLocation.getLongitude());
+            currentFuelTextview.setText(lat+", "+lng+", "+mLastUpdateTime);
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "mCurrentLocation is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean GooglePlayServicesAvailable() {
+        GoogleApiAvailability googleApi = GoogleApiAvailability.getInstance();
+        int status = googleApi.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == status) {
+            return true;
+        } else {
+            googleApi.getErrorDialog(this, status, 0).show();
+            return false;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(ActivityCompat.checkSelfPermission(this, permissions[0])== PackageManager.PERMISSION_GRANTED)
-        {
+        if (ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
 
-            switch (requestCode)
-            {
+            switch (requestCode) {
 
-                case REQUEST_LOCATION_PERMISSION:   Toast.makeText(getApplicationContext(), "Permission allowed", Toast.LENGTH_SHORT).show();
-                                                    break;
+                case REQUEST_LOCATION_PERMISSION:
+                    Toast.makeText(getApplicationContext(), "Permission allowed", Toast.LENGTH_SHORT).show();
+                    break;
 
             }
 
         }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateUI();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        startLocationUpdates();
+    }
+
+    protected void startLocationUpdates() {
+        Log.d(TAG, String.valueOf(this instanceof LocationListener));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            askForPermission(Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_LOCATION_PERMISSION);
+            return;
+        }
+        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (com.google.android.gms.location.LocationListener) this);
+        Log.d(TAG, "Location update started ..............: ");
+    }
+
+    protected void stopLocationUpdates()
+    {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, (com.google.android.gms.location.LocationListener) this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mGoogleApiClient.isConnected())
+        {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
